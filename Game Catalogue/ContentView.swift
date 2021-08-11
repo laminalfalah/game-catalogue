ddc8f2d957f60065bb5c65c5e7ace15fa3b894dd
Laminal Falah <laminalfalah08@gmail.com>
2021-08-12 04:03:46 +0700

    Initial Commit

---
+++ Game Catalogue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Game Catalogue
+//
+//  Created by Laminal Falah on 12/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
